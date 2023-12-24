9b885aeebe9448316a82c9ff8ff3f848402ac21e
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-24 17:23:35 -0300

    Add bike-related calculations to CreateRental use case

--- src/usecases/create-rental.ts
+++ src/usecases/create-rental.ts
@@ -1,15 +1,48 @@
 import { Rental } from '@/usecases/datatypes/rental';
 import { ExistingRentalError } from '@/usecases/errors/existing-rental-error';
+import { NotFoundBikeError } from '@/usecases/errors/not-found-bike-error';
+import { BikeRepository } from '@/usecases/ports/bike-repository';
 import { RentalRepository } from '@/usecases/ports/rental-repository';
 import { UseCase } from '@/usecases/ports/use-case';
 
 export class CreateRental implements UseCase {
-  constructor(private readonly rentalRepository: RentalRepository) {}
+  constructor(
+    private readonly rentalRepository: RentalRepository,
+    private readonly bikeRepository: BikeRepository
+  ) {}
 
   async perform(rental: Rental): Promise<Rental> {
     const rentalFound = await this.rentalRepository.findByDate(rental.start);
     if (rentalFound) throw new ExistingRentalError();
 
-    return this.rentalRepository.add(rental);
+    return this.rentalRepository.add({
+      ...rental,
+      subtotal: await this.calculateSubtotal(rental),
+      fee: await this.calculateFee(rental),
+      total: await this.calculateTotal(rental),
+    });
+  }
+
+  private async calculateSubtotal(rental: Rental): Promise<number> {
+    const bikeFound = await this.bikeRepository.findById(rental.bikeId);
+    if (!bikeFound) throw new NotFoundBikeError();
+
+    const { rate } = bikeFound;
+
+    const timeDiff = Math.abs(rental.end.getTime() - rental.start.getTime());
+    const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    return diffDays * rate;
+  }
+
+  private async calculateFee(rental: Rental): Promise<number> {
+    const subtotal = await this.calculateSubtotal(rental);
+    return subtotal * 0.15;
+  }
+
+  private async calculateTotal(rental: Rental): Promise<number> {
+    const subtotal = await this.calculateSubtotal(rental);
+    const fee = await this.calculateFee(rental);
+    return subtotal + fee;
   }
 }
