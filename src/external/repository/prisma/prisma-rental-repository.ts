334ba55dcf71985cbb06119574ed5aef0e6a7236
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-24 17:10:37 -0300

    Add PrismaRentalRepository implementation

---
+++ src/external/repository/prisma/prisma-rental-repository.ts
@@ -0,0 +1,29 @@
+import prismaClient from '@/external/repository/prisma/prisma-client';
+import { Rental } from '@/usecases/datatypes/rental';
+import { RentalRepository } from '@/usecases/ports/rental-repository';
+
+export class PrismaRentalRepository implements RentalRepository {
+  async findByDate(date: Date): Promise<Rental> {
+    return await prismaClient.rental.findFirst({
+      where: {
+        start: {
+          gte: date,
+        },
+        end: {
+          lte: date,
+        },
+      },
+    });
+  }
+
+  async add(rental: Rental): Promise<Rental> {
+    return prismaClient.rental.create({
+      data: {
+        bikeId: rental.bikeId,
+        userId: rental.userId,
+        start: rental.start,
+        end: rental.end,
+      },
+    });
+  }
+}
