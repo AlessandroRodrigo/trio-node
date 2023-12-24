e9da246c91a2b0d146f35ad59fcf4f013f936573
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-24 17:23:32 -0300

    Add bike repository to CreateRentalController

--- src/main/factories/make-create-rental-controller.ts
+++ src/main/factories/make-create-rental-controller.ts
@@ -1,3 +1,4 @@
+import { makeBikeRepository } from '@/main/factories/make-bike-repository';
 import { makeRentalRepository } from '@/main/factories/make-rental-repository';
 import { CreateRentalController } from '@/presentation/controllers/create-rental-controller';
 import { Controller } from '@/presentation/controllers/ports/controller';
@@ -5,6 +6,7 @@ import { CreateRental } from '@/usecases/create-rental';
 
 export const makeCreateRentalController = (): Controller => {
   const rentalRepository = makeRentalRepository();
-  const useCase = new CreateRental(rentalRepository);
+  const bikeRepository = makeBikeRepository();
+  const useCase = new CreateRental(rentalRepository, bikeRepository);
   return new CreateRentalController(useCase);
 };
