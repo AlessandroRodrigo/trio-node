b124c307c981d8c0e30c9b54fb95ec60b0cdfdc2
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-25 22:04:05 -0300

    Refactor CreateRentalController to handle user authorization

--- src/presentation/controllers/create-rental-controller.ts
+++ src/presentation/controllers/create-rental-controller.ts
@@ -7,12 +7,25 @@ export class CreateRentalController implements Controller {
   async handle(request: HttpRequest): Promise<HttpResponse> {
     try {
       const rental = request.body;
-      const rentalCreated = await this.createRentalUseCase.perform(rental);
+      const candidateToken = request.token;
+      const rentalCreated = await this.createRentalUseCase.perform(rental, candidateToken);
       return {
         statusCode: 201,
         body: rentalCreated,
       };
     } catch (error) {
+      const userUnauthorized = error.constructor.name === 'UnauthorizedError';
+
+      if (userUnauthorized) {
+        return {
+          statusCode: error.httpStatus,
+          body: {
+            errorType: error.constructor.name,
+            message: error.message,
+          },
+        };
+      }
+
       return {
         statusCode: 500,
         body: error,
