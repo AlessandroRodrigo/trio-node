5f13bb7ec22cb501668a5bb658e3c52fb4eb37cc
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-25 22:04:08 -0300

    Add candidate repository and authorization check to CreateRental use case

--- src/usecases/create-rental.ts
+++ src/usecases/create-rental.ts
@@ -1,17 +1,23 @@
 import { Rental } from '@/usecases/datatypes/rental';
 import { ExistingRentalError } from '@/usecases/errors/existing-rental-error';
 import { NotFoundBikeError } from '@/usecases/errors/not-found-bike-error';
+import { UnauthorizedError } from '@/usecases/errors/unauthorized-error';
 import { BikeRepository } from '@/usecases/ports/bike-repository';
+import { CandidateRepository } from '@/usecases/ports/candidate-repository';
 import { RentalRepository } from '@/usecases/ports/rental-repository';
 import { UseCase } from '@/usecases/ports/use-case';
 
 export class CreateRental implements UseCase {
   constructor(
     private readonly rentalRepository: RentalRepository,
-    private readonly bikeRepository: BikeRepository
+    private readonly bikeRepository: BikeRepository,
+    private readonly candidateRepository: CandidateRepository
   ) {}
 
-  async perform(rental: Rental): Promise<Rental> {
+  async perform(rental: Rental, candidateToken: string): Promise<Rental> {
+    const candidate = await this.candidateRepository.findByToken(candidateToken);
+    if (!candidate) throw new UnauthorizedError();
+
     const rentalFound = await this.rentalRepository.findByDate(rental.start);
     if (rentalFound) throw new ExistingRentalError();
 
