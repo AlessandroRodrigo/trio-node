bf6848df3bda5448f8d2776b1b7af78f54a556b7
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-25 22:04:02 -0300

    Add candidate repository to CreateRental use case

--- src/main/factories/make-create-rental-controller.ts
+++ src/main/factories/make-create-rental-controller.ts
@@ -1,4 +1,5 @@
 import { makeBikeRepository } from '@/main/factories/make-bike-repository';
+import { makeCandidateRepository } from '@/main/factories/make-candidate-repository';
 import { makeRentalRepository } from '@/main/factories/make-rental-repository';
 import { CreateRentalController } from '@/presentation/controllers/create-rental-controller';
 import { Controller } from '@/presentation/controllers/ports/controller';
@@ -7,6 +8,7 @@ import { CreateRental } from '@/usecases/create-rental';
 export const makeCreateRentalController = (): Controller => {
   const rentalRepository = makeRentalRepository();
   const bikeRepository = makeBikeRepository();
-  const useCase = new CreateRental(rentalRepository, bikeRepository);
+  const candidateRepository = makeCandidateRepository();
+  const useCase = new CreateRental(rentalRepository, bikeRepository, candidateRepository);
   return new CreateRentalController(useCase);
 };
