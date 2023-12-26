a6e3f3bd2c7d79228c597942b27d38e46c3e098e
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-26 13:12:55 -0300

    Update rental-builder.ts to use candidateId instead of userId

--- test/builders/rental-builder.ts
+++ test/builders/rental-builder.ts
@@ -3,7 +3,7 @@ import { Rental } from '@/usecases/datatypes/rental';
 export class RentalBuilder {
   private rental: Rental = {
     bikeId: 0,
-    userId: 0,
+    candidateId: 0,
     start: new Date(),
     end: new Date(new Date().setDate(new Date().getDate() + 5)),
     fee: 10,
@@ -21,8 +21,8 @@ export class RentalBuilder {
     return this;
   }
 
-  withUserId(id: number): RentalBuilder {
-    this.rental.userId = id;
+  withCandidateId(id: number): RentalBuilder {
+    this.rental.candidateId = id;
     return this;
   }
 
