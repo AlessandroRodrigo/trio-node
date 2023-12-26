c38c73559c3103baf20a0ba7dce82b2b99c8fa2a
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-25 21:00:51 -0300

    Add RentalBuilder class for creating rental objects

---
+++ test/builders/rental-builder.ts
@@ -0,0 +1,32 @@
+import { Rental } from '@/usecases/datatypes/rental';
+
+export class RentalBuilder {
+  private rental: Rental = {
+    bikeId: 0,
+    userId: 0,
+    end: new Date(),
+    start: new Date(),
+    fee: 10,
+    subtotal: 10,
+    total: 10,
+  };
+
+  withId(): RentalBuilder {
+    this.rental.id = 1;
+    return this;
+  }
+
+  withBikeId(id: number): RentalBuilder {
+    this.rental.bikeId = id;
+    return this;
+  }
+
+  withUserId(id: number): RentalBuilder {
+    this.rental.userId = id;
+    return this;
+  }
+
+  build(): Rental {
+    return this.rental;
+  }
+}
