6a6e3852b6a77b3ee705cb1277c78d245a90f3b2
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-25 21:23:39 -0300

    Add unit test for CreateRental use case

---
+++ test/usecases/create-rental.spec.ts
@@ -0,0 +1,49 @@
+import { CreateRental } from '@/usecases/create-rental';
+import { BikeBuilder } from '@test/builders/bike-builder';
+import { CandidateBuilder } from '@test/builders/candidate-builder';
+import { RentalBuilder } from '@test/builders/rental-builder';
+import { InMemoryBikeRepository } from '@test/doubles/in-memory-bike-repository';
+import { InMemoryCandidateRepository } from '@test/doubles/in-memory-candidate-repository';
+import { InMemoryRentalRepository } from '@test/doubles/in-memory-rental-repository';
+import { InMemoryUserRepository } from '@test/doubles/in-memory-user-repository';
+
+describe('Create rental use case', () => {
+  it.only('should create a rental given the dates', async () => {
+    const candidateRepository = new InMemoryCandidateRepository();
+    const bikeRepository = new InMemoryBikeRepository();
+    const userRepository = new InMemoryUserRepository();
+    const rentalRepository = new InMemoryRentalRepository();
+    const useCase = new CreateRental(rentalRepository, bikeRepository);
+
+    const addedCandidate = new CandidateBuilder().withToken().build();
+    const candidate = await candidateRepository.add(addedCandidate);
+    const user = await userRepository.add({
+      candidateId: candidate.id,
+      email: 'any_email',
+      name: 'any_name',
+      password: 'any_password',
+    });
+    const bike = new BikeBuilder().build();
+    console.log('bike', bike);
+    const differentBike = new BikeBuilder().different().build();
+    const bikesAdded = await Promise.all([
+      bikeRepository.add({ id: 1, candidateId: candidate.id, ...bike }),
+      bikeRepository.add({ id: 2, candidateId: candidate.id, ...differentBike }),
+    ]);
+    const rentalInfo = new RentalBuilder().withBikeId(bikesAdded[0].id).withUserId(user.id).build();
+    const rentalCreated = await useCase.perform(rentalInfo);
+
+    console.log(rentalCreated);
+
+    expect(rentalCreated).toEqual({
+      id: expect.any(Number),
+      bikeId: bikesAdded[0].id,
+      userId: user.id,
+      start: rentalInfo.start,
+      end: rentalInfo.end,
+      fee: 7.5,
+      subtotal: 50,
+      total: 57.5,
+    });
+  });
+});
